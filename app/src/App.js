881c0b786825dbe93db48c070d5c360665bc31da
theleoad <talkto@theleoad.com>
2019-05-17 10:45:40 -0300

    Executar desafios da Aula 01

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import BtnBotcamp from "./components/BtnBotcamp";
+import LogoBotcamp from "./components/LogoBotcamp";
+import "./Styles/reset.css";
+import "./Styles/style.css";
 
 class App extends Component {
   render() {
-    return <BtnBotcamp />;
+    return (
+      <div className="container">
+        <LogoBotcamp />
+        <BtnBotcamp />
+      </div>
+    );
   }
 }
 
---
+++ app/src/components/LogoBotcamp/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const LogoBotcamp = () => (
+  <img src="/images/logo-botcamp.png" alt="Logo BotCamp" />
+);
+
+export default LogoBotcamp;
