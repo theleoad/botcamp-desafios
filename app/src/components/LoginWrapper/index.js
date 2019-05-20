c9eadcb86a1351187bfcb49de418182cfd22edf2
theleoad <talkto@theleoad.com>
2019-05-20 09:53:13 -0300

    Corrigindo posicionamento

--- app/src/components/LoginWrapper/index.js
+++ app/src/components/LoginWrapper/index.js
@@ -3,14 +3,16 @@ import styled from "styled-components";
 import background from "../../img/botcamp-bg.png";
 
 const LoginWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
   width: 100vw;
   height: 100vh;
   background-image: url(${background});
   background-size: cover;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
+  padding: 120px 0 80px;
 `;
 
 export default LoginWrapper;
